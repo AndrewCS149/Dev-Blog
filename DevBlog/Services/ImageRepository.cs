c3b7b1acb82223f3af8636aca090aa33e8eb9530
Andrew Smith <andrewcs149@gmail.com>
2022-08-30 14:42:47 -0700

    Add services

---
+++ DevBlog/Services/ImageRepository.cs
@@ -0,0 +1,57 @@
+﻿using DevBlog.Interfaces;
+using Dropbox.Api;
+using Dropbox.Api.Files;
+
+namespace DevBlog.Services
+{
+    public class ImageRepository : IImageRepository
+    {
+        private readonly IConfiguration _config;
+
+        public ImageRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        /// <summary>
+        /// Uploads an image to Dropbox account
+        /// </summary>
+        /// <param name="fs">Image file stream</param>
+        /// <param name="name">Name of image</param>
+        /// <returns>Dropbox url to image</returns>
+        public async Task<string> AddImgToDropBox(Stream fs, string name)
+        {
+            // create unique file name
+            string ext = Path.GetExtension(name);
+            string fileName = $"{DateTime.Now.Ticks}{name}{ext}";
+
+
+            var destinationPath = _config["DropboxDestinationPath"];
+
+            string url = "";
+            string dest = destinationPath + fileName;
+
+            using (var dbx = new DropboxClient(_config["DropboxToken"]))
+            {
+                // upload file to dbx
+                var updated = await dbx.Files.UploadAsync(
+                    dest,
+                    WriteMode.Overwrite.Instance,
+                    body: fs
+                );
+                fs.Close();
+
+                // create shareable link
+                var link = dbx.Sharing.CreateSharedLinkWithSettingsAsync(dest);
+                link.Wait();
+
+                url = link.Result.Url;
+
+                // remove id and replace with raw=1
+                url = url.Substring(0, url.Length - 4) + "raw=1";
+            }
+
+            return url;
+        }
+    }
+}
---
+++ DevBlog/Services/VoteRepository.cs
@@ -0,0 +1,86 @@
+﻿using DevBlog.Data;
+using DevBlog.Interfaces;
+using DevBlog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBlog.Services
+{
+    public class VoteRepository : IVoteRepository
+    {
+        private AppDbContext _db;
+
+        public VoteRepository(AppDbContext context)
+        {
+            _db = context;
+        }
+
+        /// <summary>
+        /// Adds an up vote
+        /// </summary>
+        /// <param name="vote"></param>
+        /// <returns>Successful completion of task</returns>
+        public async Task<UpVoteModel> UpVote(int postId, string username)
+        {
+            var downVote = await _db.DownVote.Where(x => x.PostModelId == postId &&
+                          x.UserName == username)
+                           .FirstOrDefaultAsync();
+
+            var upVote = await _db.UpVote.Where(v => v.PostModelId == postId &&
+                          v.UserName == username)
+                           .FirstOrDefaultAsync();
+
+            if (downVote != null)
+                _db.Remove(downVote);
+            else if (upVote != null)
+            {
+                _db.Remove(upVote);
+                await _db.SaveChangesAsync();
+                return null;
+            }
+
+            var newVote = new UpVoteModel()
+            {
+                PostModelId = postId,
+                UserName = username
+            };
+
+            _db.Add(newVote);
+            await _db.SaveChangesAsync();
+            return newVote;
+        }
+
+        /// <summary>
+        /// Adds a down vote
+        /// </summary>
+        /// <param name="vote"></param>
+        /// <returns>Successful completion of task</returns>
+        public async Task<DownVoteModel> DownVote(int postId, string username)
+        {
+            var downVote = await _db.DownVote.Where(x => x.PostModelId == postId &&
+                          x.UserName == username)
+                           .FirstOrDefaultAsync();
+
+            var upVote = await _db.UpVote.Where(v => v.PostModelId == postId &&
+                          v.UserName == username)
+                           .FirstOrDefaultAsync();
+
+            if (upVote != null)
+                _db.Remove(upVote);
+            else if (downVote != null)
+            {
+                _db.Remove(downVote);
+                await _db.SaveChangesAsync();
+                return null;
+            }
+            var newVote = new DownVoteModel()
+            {
+                PostModelId = postId,
+                UserName = username
+            };
+
+            _db.Add(newVote);
+            await _db.SaveChangesAsync();
+            return newVote;
+        }
+    }
+}
