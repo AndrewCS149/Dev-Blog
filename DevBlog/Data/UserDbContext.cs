903ad6a6eaf7f9c4c19634ca99a74c9aab6c76a7
Andrew Smith <andrewcs149@gmail.com>
2022-08-30 14:40:18 -0700

    Add DbContext

---
+++ DevBlog/Data/UserDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DevBlog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBlog.Data
+{
+    public class UserDbContext : IdentityDbContext<UserModel>
+    {
+        public UserDbContext(DbContextOptions opt) : base(opt)
+        {
+        }
+    }
+}
