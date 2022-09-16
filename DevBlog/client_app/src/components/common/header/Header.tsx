d5900a283a7424af612552bba8867590662c60cf
Andrew Smith <andrewcs149@gmail.com>
2022-09-16 15:13:29 -0700

    Modify header

--- DevBlog/client_app/src/components/common/header/Header.tsx
+++ DevBlog/client_app/src/components/common/header/Header.tsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+import { Navbar } from '../../common';
+import './Header.css';
+
 const Header = () => {
     return (
-        <div>
-            HEADER
+        <div className="header">
+            <div className="header-logo">
+                <Link to="/">
+                    <img src="Logo.png" alt="Picture of Logo" />
+                </Link>
+            </div>
+            <div className="header-nav">
+                <Navbar />
+            </div>
+            <div>
+                Login SignUp
+            </div>
         </div>
     )
 }
